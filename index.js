b4fa7db7978fab469895d175a12c69fc44acc255
morkalork <maffelu@gmail.com>
2018-02-24 15:32:26 +0100

    Fixed case management.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 /**
  * Pixafont will return the coordinates needed to draw a letter or a number
- * @param {string} letter The letter you want to generate in pixel form
+ * @param {string} character The character you want to generate in pixel form
  * @param {number} startX The start X position
  * @param {number} startY The start Y position
  * @returns {array} An array of coordinates describing the letter
  */
-module.exports = (letter, startX = 0, startY = 0) => {
-  if(!/[a-z0-9]/.test(letter)) {
+module.exports = (character, startX = 0, startY = 0) => {
+  const safeCharacter = character.toLowerCase();
+  if(!/[a-z0-9]/.test(safeCharacter)) {
     throw new Error('The letter must be a letter, not something un-lettery.');
   }
 
@@ -18,7 +19,7 @@ module.exports = (letter, startX = 0, startY = 0) => {
     throw new Error(`Y must be a number, you sent in a "${typeof startY}"`);
   }
 
-  const letterArray = require(`./characters/${letter}.js`);
+  const letterArray = require(`./characters/${safeCharacter}.js`);
   return letterArray.map((letter) => [
     startX > 0 ? letter[0] * startX : letter[0], 
     startY > 0 ? letter[1] * startY : letter[1]
